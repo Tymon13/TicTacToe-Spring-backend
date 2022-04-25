a44f91bd106d3ccb9fa3890f077d999eb4443485
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-04-25 10:46:14 +0200

    Add PlayerEntity class

---
+++ src/main/java/com/example/demo/PlayerEntity.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "player")
+public class PlayerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    private String name;
+}
