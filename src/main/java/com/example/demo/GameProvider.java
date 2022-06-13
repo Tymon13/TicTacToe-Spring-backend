5594733dc95f6d1d42d216f3221945b0d78c17f0
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-06-13 21:07:51 +0200

    Remove leftover classes that are not used anymore

--- src/main/java/com/example/demo/GameProvider.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.exception.GameDoesntExistException;
-
-public interface GameProvider {
-    int makeNewGame();
-
-    GameDao getExistingGame(int id) throws GameDoesntExistException;
-}
