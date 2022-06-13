24b0ac0bb1cf146528a30d0b3f2744573a1acd4c
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-06-13 21:07:15 +0200

    Fix integration test after removing Player enum

--- src/test/java/com/example/demo/MonographicApplicationTests.java
+++ src/test/java/com/example/demo/MonographicApplicationTests.java
@@ -18,55 +18,68 @@ class MonographicApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    private int startNewGame() throws Exception {
-        MvcResult result = mockMvc.perform(post("/game/new"))
+    private int createNewPlayer() throws Exception {
+        MvcResult result = mockMvc.perform(post("/player/new?name=test_username"))
+                .andExpect(status().isOk()).andReturn();
+        String body = result.getResponse().getContentAsString();
+        return JsonPath.read(body, "$.id");
+    }
+
+    private int startNewGame(int player1Id, int player2Id) throws Exception {
+        MvcResult result = mockMvc.perform(post(String.format("/game/new?player1=%d&player2=%d", player1Id, player2Id)))
                 .andExpect(status().isOk())
                 .andReturn();
         String body = result.getResponse().getContentAsString();
         return JsonPath.read(body, "$.id");
     }
 
-    private void playMove(int gameId, Player p, int x, int y) throws Exception {
-        mockMvc.perform(put(String.format("/game/%d/play?player=%s&x=%d&y=%d", gameId, p.toString(), x, y)))
+    private void playMove(int gameId, int p, int x, int y) throws Exception {
+        mockMvc.perform(put(String.format("/game/%d/play?player=%s&x=%d&y=%d", gameId, p, x, y)))
                 .andExpect(status().isOk());
     }
 
-    private void checkWinner(int gameId, Player expectedWinner) throws Exception {
+    private void checkWinner(int gameId, int expectedWinner) throws Exception {
         mockMvc.perform(get(String.format("/game/%d/winner", gameId)))
                 .andExpect(status().isOk())
-                .andExpect(content().string(String.format("\"%s\"", expectedWinner.toString())));
+                .andExpect(content().json(String.format("{\"gameId\":%d, \"playerId\":%d}", gameId, expectedWinner)));
     }
 
     @Test
     void runSimpleGame() throws Exception {
-        int gameId = startNewGame();
-        playMove(gameId, Player.Player2, 9, 9);
-        playMove(gameId, Player.Player2, 8, 8);
-        playMove(gameId, Player.Player2, 7, 7);
-        playMove(gameId, Player.Player2, 6, 6);
-        playMove(gameId, Player.Player2, 5, 5);
-        checkWinner(gameId, Player.Player2);
+        int player1Id = createNewPlayer();
+        int player2Id = createNewPlayer();
+        int gameId = startNewGame(player1Id, player2Id);
+        playMove(gameId, player2Id, 9, 9);
+        playMove(gameId, player2Id, 8, 8);
+        playMove(gameId, player2Id, 7, 7);
+        playMove(gameId, player2Id, 6, 6);
+        playMove(gameId, player2Id, 5, 5);
+        checkWinner(gameId, player2Id);
     }
 
     @Test
     void playTwoGamesAtTheSameTime() throws Exception {
-        int gameId1 = startNewGame();
-        playMove(gameId1, Player.Player2, 9, 1);
-        playMove(gameId1, Player.Player2, 8, 2);
-        playMove(gameId1, Player.Player2, 7, 3);
+        int player1Id = createNewPlayer();
+        int player2Id = createNewPlayer();
+        int player3Id = createNewPlayer();
 
-        int gameId2 = startNewGame();
-        playMove(gameId2, Player.Player1, 1, 1);
-        playMove(gameId2, Player.Player1, 2, 2);
+        int gameId1 = startNewGame(player1Id, player2Id);
+        playMove(gameId1, player2Id, 9, 1);
+        playMove(gameId1, player2Id, 8, 2);
+        playMove(gameId1, player2Id, 7, 3);
 
-        playMove(gameId1, Player.Player2, 6, 4);
-        playMove(gameId1, Player.Player2, 5, 5);
+        int gameId2 = startNewGame(player1Id, player3Id);
+        playMove(gameId2, player1Id, 1, 1);
+        playMove(gameId2, player1Id, 2, 2);
 
-        playMove(gameId2, Player.Player1, 3, 3);
-        playMove(gameId2, Player.Player1, 4, 4);
-        playMove(gameId2, Player.Player1, 5, 5);
+        playMove(gameId1, player2Id, 6, 4);
+        playMove(gameId1, player2Id, 5, 5);
 
-        checkWinner(gameId1, Player.Player2);
-        checkWinner(gameId2, Player.Player1);
+        playMove(gameId2, player1Id, 3, 3);
+        playMove(gameId2, player1Id, 4, 4);
+        playMove(gameId2, player1Id, 5, 5);
+
+        checkWinner(gameId1, player2Id);
+        checkWinner(gameId2, player1Id);
     }
 }
